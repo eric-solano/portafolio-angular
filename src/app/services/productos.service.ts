69bf03cd254295a74f700207fb89227a85034c01
Eric Solano <eric.solano.levano@gmail.com>
2020-04-15 00:01:44 -0500

    Fin seccion 7

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Producto } from '../interfaces/producto.interface';
 
-
 @Injectable({
   providedIn: 'root'
 })
@@ -10,21 +9,62 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productoFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) { 
     this.cargarProducto();
   }
 
   private cargarProducto(){
-    this.http.get('https://angular-html-22c21.firebaseio.com/productos_idx.json').
-      subscribe((resp: Producto[]) =>{
-        console.log(resp);
-        this.productos = resp;
-       
-        setTimeout(()=>{
-          this.cargando = false;
-        },2000);
+    
+    return new Promise((resolve,reject) => {
 
-      });
+      this.http.get('https://angular-html-22c21.firebaseio.com/productos_idx.json').
+        subscribe((resp: Producto[]) =>{
+          console.log(resp);
+          this.productos = resp;        
+          //setTimeout(()=>{
+            this.cargando = false;
+          //},2000);
+          resolve();
+        });
+    });
   }
+
+  getProducto(id:string){
+    //`: permite hacer insersiones dentro de string
+    return this.http.get(`https://angular-html-22c21.firebaseio.com/productos/${id}.json`);
+  }
+
+  buscarProducto(termino:string){
+    if(this.productos.length === 0){
+      //cargar productos
+      this.cargarProducto().then(()=>{
+        //ejecutar despues de tener los productos
+        //aplicar filtros
+        this.filtrarProductos(termino);
+      });
+    }else {
+      this.filtrarProductos(termino);
+    }
+  }
+
+  private filtrarProductos(termino: string){
+    console.log(this.productos);
+    this.productoFiltrado = [];
+    termino = termino.toLocaleLowerCase();
+
+    this.productos.forEach(prod=>{
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+
+      if(prod.categoria.indexOf(termino)>=0 || tituloLower.indexOf(termino)>=0)
+      {
+        this.productoFiltrado.push(prod);
+      }
+    });
+
+    
+
+  }  
+
 }
